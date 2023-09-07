117de694b4c5d228e6f7617e15d7652d49efecd2
Melozzo <carmecamma1995@gmail.com>
2023-09-07 14:57:34 +0200

    [create]: instruction in script js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// 1. salvare chilometri da percorrere ed età del passeggero in prompt
+// 2. applicare sconto per le varie età
+// 3. calcolare prezzo biglietto
+// 4. mandare in stampa resoconto
+
