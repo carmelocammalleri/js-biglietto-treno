1c736542a2c4e30dd14f8815c4d96ca31eaf4f13
Melozzo <carmecamma1995@gmail.com>
2023-09-07 15:58:56 +0200

    [add]: ifNan and first sale

--- js/script.js
+++ js/script.js
@@ -3,6 +3,26 @@
 // 3. calcolare prezzo biglietto
 // 4. mandare in stampa resoconto
 
+// 1.
 const travelKm = prompt ('Quanti chilometri devi percorrere?');
 const age = prompt ('Quanti anni hai?');
 const priceKm = 0.21;
+const priceTicket = (travelKm * priceKm);
+
+let message = `Il prezzo del biglietto è di ${priceTicket}`
+let error;
+
+
+if(isNaN(age && travelKm)){
+  error = 'Errore! Non hai inserito un numero';
+}
+
+
+// 2.
+if(age < 18){
+  console.log ('Utente ha 20% di sconto')
+}else (age > 64)
+
+
+document.getElementById
+console.log(error);
