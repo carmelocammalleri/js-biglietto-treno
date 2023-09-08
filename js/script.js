52f3b0af1ba0e0408593d58af9e913c9bdf0d939
Melozzo <carmecamma1995@gmail.com>
2023-09-08 10:26:17 +0200

    [fix]: price

--- js/script.js
+++ js/script.js
@@ -3,30 +3,31 @@
 // 3. mandare in stampa resoconto
 
 // 1.
-const travelKm = prompt ('Quanti chilometri devi percorrere?');
-const age = prompt ('Quanti anni hai?');
+const travelKm = parseInt (prompt ('Quanti chilometri devi percorrere?'));
+const age = parseInt (prompt ('Quanti anni hai?')) ;
 const priceKm = 0.21;
 const priceTicket = (travelKm * priceKm);
 
 
 // messages
-let message = `Il prezzo del biglietto è di ${priceTicket}€`
+let message = `Il prezzo del biglietto è di ${priceTicket.toFixed(2)}€`
 let error;
+let insertOk = true
 
 
 // verify
-if(isNaN(age && travelKm)){
-  error = 'Errore! Non hai inserito un numero';
+if(isNaN(age || travelKm)){
+  let insertOk = false
 }
 
 
 // 2.
 if(age < 18){
   saleUnder = (priceTicket - priceTicket *20 / 100);
-  messageUnder = (`Il prezzo del biglietto per l'utente under-18 è di ${saleUnder}€`);
+  messageUnder = (`Il prezzo del biglietto per l'utente under-18 è di ${saleUnder.toFixed(2)}€`);
 } else if(age > 64){
   saleOver = (priceTicket - priceTicket *40 /100)
-  messageOver = (`Il prezzo del biglietto per l'utente over-64 è di ${saleUnder}€`);
+  messageOver = (`Il prezzo del biglietto per l'utente over-64 è di ${saleUnder.toFixed(2)}€`);
 }
 
 
